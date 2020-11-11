b187ee51ef9783200276f95a8dfbe8faf6d5163c
YvanDotet <40297112+YvanDotet@users.noreply.github.com>
2020-11-11 19:35:40 +0100

    Add files via upload

---
+++ query_deluxe/report/print_pdf.py
@@ -0,0 +1,15 @@
+from odoo import models, fields, api
+
+
+class PrintPdfParser(models.AbstractModel):
+    _name = 'report.query_deluxe.pdf'
+    _description = 'Parser for my pdf result'
+
+    def _get_report_values(self, docids, data=None):
+        # append more informations to data
+        print(docids)
+        print(data)
+
+        return {
+
+        }
