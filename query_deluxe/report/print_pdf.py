99812e7a43a63a8771a16c2d27d58d54c8578b3b
YvanDotet <40297112+YvanDotet@users.noreply.github.com>
2020-11-28 15:30:20 +0100

    Update print_pdf.py

--- query_deluxe/report/print_pdf.py
+++ query_deluxe/report/print_pdf.py
@@ -5,6 +5,7 @@ class PrintPdfParser(models.AbstractModel):
     _name = 'report.query_deluxe.pdf'
     _description = 'Parser for my pdf result'
 
+    @api.model
     def _get_report_values(self, docids, data=None):
         # append more informations to data
         print(docids)
