ff62e851a2c17d19e9de600810af1927a00ac6c8
YvanDotet <40297112+YvanDotet@users.noreply.github.com>
2022-12-30 15:16:57 +0100

    Update __manifest__.py

--- query_deluxe/__manifest__.py
+++ query_deluxe/__manifest__.py
@@ -4,7 +4,7 @@
         'author': 'Yvan Dotet',
         'depends': ['base', 'mail'],
         'application': True,
-        'version': '16.0.1.0.4',
+        'version': '16.0.1.0.5',
         'license': 'AGPL-3',
         'support': 'yvandotet@yahoo.fr',
         'website': 'https://github.com/YvanDotet/query_deluxe/',
